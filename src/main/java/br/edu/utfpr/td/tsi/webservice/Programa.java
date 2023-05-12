956aed42ccddd2fae842202a9d9868bc24090698
Marcos <marcossouzatol@gmail.com>
2023-05-11 23:22:10 -0300

    lendo arquivo csv completo

--- src/main/java/br/edu/utfpr/td/tsi/webservice/Programa.java
+++ src/main/java/br/edu/utfpr/td/tsi/webservice/Programa.java
@@ -16,28 +16,28 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
 import br.edu.utfpr.td.tsi.webservice.modelo.BoletimFurtoVeiculo;
+import br.edu.utfpr.td.tsi.webservice.modelo.Emplacamento;
 import br.edu.utfpr.td.tsi.webservice.modelo.Endereco;
-import br.edu.utfpr.td.tsi.webservice.modelo.Parte;
 import br.edu.utfpr.td.tsi.webservice.modelo.Veiculo;
 
 @SpringBootApplication
 @ComponentScan("br.edu.utfpr.td.tsi.webservice")
 public class Programa {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Programa.class, args);
 
-		String path = "C:\\temp\\furtos.csv";
+		String path = "C:\\pasta\\temp\\furtos.csv";
 		readDataLineByLine(path);
-		
-		List<BoletimFurtoVeiculo> boletins = new ArrayList<>();
 		
 	}
 
 	// Java code to illustrate reading a
 	// CSV file line by line
 	public static void readDataLineByLine(String file) {
-
+		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+		List<BoletimFurtoVeiculo> boletins = new ArrayList<>();
+		
 		try {
 
 			// Create an object of filereader
@@ -49,31 +49,47 @@ public class Programa {
 			// file reader as a parameter
 			CSVParser parser = new CSVParserBuilder().withSeparator('\t').build();
 			CSVReader csvReader = new CSVReaderBuilder(filereader).withCSVParser(parser).build();
-			String[] linha;
+			String[] coluna;
+			csvReader.readNext();
 
 			// we are going to read data line by line
-			while ((linha = csvReader.readNext()) != null) {
+			while ((coluna = csvReader.readNext()) != null) {
 				BoletimFurtoVeiculo boletim = new BoletimFurtoVeiculo();
-				boletim.setIdentificador(linha[1]);
-				boletim.setPeriodoOcorrencia(linha[7]);
+				Endereco endereco = new Endereco();
+				Veiculo veiculo = new Veiculo();
+				Emplacamento emplacamento = new Emplacamento();
+				boletim.setIdentificador(coluna[2]);
+				Date data = formato.parse(coluna[5]);
+				boletim.setDataOcorrencia(data);
+				boletim.setPeriodoOcorrencia(coluna[7]);
+				boletim.setParte(null);
+				endereco.setLogradouro(coluna[13]);
+				endereco.setNumero(Integer.parseInt(coluna[14]));
+				endereco.setBairro(coluna[15]);
+				endereco.setCidade(coluna[16]);
+				endereco.setEstado(coluna[17]);
+				boletim.setLocalOcorrencia(endereco);
+				emplacamento.setPlaca(coluna[44]);
+				emplacamento.setEstado(coluna[45]);
+				emplacamento.setCidade(coluna[46]);
+				veiculo.setEmplacamento(emplacamento);
+				veiculo.setCor(coluna[47]);
+				veiculo.setMarca(coluna[48]);
+				if(coluna[49] == "")
+					veiculo.setAnoFabricacao(0);
+				else
+				veiculo.setAnoFabricacao(Integer.parseInt(coluna[49]));
+				veiculo.setTipoVeiculo(coluna[51]);
+				boletim.setVeiculoFurtado(veiculo);
+				
 				System.out.println(boletim);
-				
-				/*
-				SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-                Date dataOcorrencia = format.parse(colunas[1]);
-				
-				 Endereco endereco = new Endereco(colunas[4], colunas[5], colunas[6], colunas[7], colunas[8]);
-				 Veiculo veiculo = new Veiculo(colunas[9], colunas[10], colunas[11], colunas[12], colunas[13]);
-				 Parte parte = new Parte(colunas[14], colunas[15], colunas[16], colunas[17]);
-				 
-				 BoletimFurtoVeiculo boletim = new BoletimFurtoVeiculo(colunas[0], dataOcorrencia, colunas[2], parte, endereco, veiculo);
-				 */
-
 
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		
 	}
 
 }
